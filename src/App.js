a98a724cacbd2873424d51f1568af5c7de7f761b
Sangam Rai <122705359+sangamrai07@users.noreply.github.com>
2023-06-23 14:15:44 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Clock from './Clock';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
